e5085d518044a07c36ad327a61856d2c71705dcd
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-30 13:23:19 -0800

    remove react-hot-loader import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -11,11 +10,11 @@ const store = createStore(rootReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-      <HashRouter>
-        <Provider store={store}>
-          <Component/>
-        </Provider>
-      </HashRouter>,
+    <HashRouter>
+      <Provider store={store}>
+        <Component/>
+      </Provider>
+    </HashRouter>,
     document.getElementById('react-app-root')
   );
 };
