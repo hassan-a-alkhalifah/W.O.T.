3b473cd04571bc7837cc2f5a4a43204cb62673d6
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-30 13:25:53 -0800

    import react, reactor-router-dom and components Error404, HomePage, StartNewWorkout and ExerciseArchives

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import StartNewWorkout from './StartNewWorkout';
+import ExerciseArchives from './ExerciseArchives';
 
 function App() {
   return(
-    <div>App</div>
+    <div>
+      <h1>App</h1>
+      <Switch>
+        <Route exact path='/' render={()=><HomePage />} />
+        <Route exact path='/startNewWorkout' render={()=><StartNewWorkout />} />
+        <Route exact path='/exerciseArchives' render={()=><ExerciseArchives />} />
+        <Route component={Error404}/>
+      </Switch>
+    </div>
   );
 }
 
