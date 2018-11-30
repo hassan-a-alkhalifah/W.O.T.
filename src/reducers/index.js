5f4fbd290dde854d9fe8826dfde31f2d5ae46238
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-30 13:31:53 -0800

    import initialReducer reducer and declare in combinedReducer method as state slice

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import initialReducer from './initial-reducer';
 
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({
+  initialReducer: initialReducer
+});
 
 export default rootReducer;
