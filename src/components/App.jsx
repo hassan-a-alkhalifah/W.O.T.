51c02459923e3f81d7bae8031ebfeb9e81a7b910
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-30 12:25:45 -0800

    add components directory to include App.jsx component, add constants directory to include ActionTypes.js and index.js, add reducers directoryto include index.js, add index.js which is set up to create initial store, render componet, provide HashRouter and Provider which provides store to all children components

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return(
+    <div>App</div>
+  );
+}
+
+export default App;
