d1bd0bb06f848ba1dcd1207220e407985ea8e30b
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-07 14:57:48 -0800

    add workout labels and relevant styles

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -23,7 +23,7 @@ function Exercise() {
     alignItems: 'center'
   }
   const addSetIconSpacerStyles = {
-    width: '230px',
+    width: '240px',
     height: '34px'
   }
   const addSetIconStyles = {
@@ -38,14 +38,42 @@ function Exercise() {
             width: 185px;
             height: 34px;
             border: 1px solid #3498DB;
+            padding-left: 13px;
           }
           input[type=text]::placeholder {
             color: #C7C5C5;
             font-size: 14px;
           }
+          #exerciseLabelsContainer {
+            width: 198px;
+            height: 34px;
+            border: 1px solid #3498DB;
+            margin-top: -1px;
+            display: flex;
+            justify-content: center;
+          }
+          #exerciseLabelsContainer div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
 
-
-
+            font-size: 14px;
+            font-weight: 400;
+          }
+          #exerciseLabelsContainer div p {
+            font-weight: 400;
+          }
+          #exerciseLabelsContainer div:nth-child(1) {
+            width: 58px;
+          }
+          #exerciseLabelsContainer div:nth-child(2) {
+            width: 80px;
+            border-left: 1px solid #3498DB;
+            border-right: 1px solid #3498DB;
+          }
+          #exerciseLabelsContainer div:nth-child(3) {
+            width: 58px;
+          }
           input[type="checkbox"] + span,
           input[type="checkbox"] + span::before
           {
@@ -87,6 +115,17 @@ function Exercise() {
           <input type="checkbox" name="checkbox-02" />
           <span></span>
         </label>
+      </div>
+      <div id="exerciseLabelsContainer">
+        <div>
+          <p>Sets</p>
+        </div>
+        <div>
+          <p>Weight</p>
+        </div>
+        <div>
+          <p>Reps</p>
+        </div>
       </div>
       <SetList></SetList>
       <div style={addSetIconContainerStyles}>
