f9db5526b9963b305eb439fa5f2ca69a0c701362
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-07 10:31:37 -0800

    import commentIcon and githubIcon, add footerStyles and img inline styles, add cooded and designed by p tag along with commentIcon and githubIcon img tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import commentIcon from '../assets/images/comment-icon.png';
+import githubIcon from '../assets/images/github-icon.png';
 
 function Footer() {
+  const footerStyles = {
+    width: '100%',
+    height: '47px',
+    backgroundColor: '#000',
+    position: 'absolute',
+    bottom: '0',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    color: '#FFF',
+    fontSize: '12px'
+  }
+
   return(
-    <div>Footer</div>
+    <div style={footerStyles}>
+      <style jsx>{`
+          img {
+            width: 21px;
+          }
+      `}</style>
+      <p>Coded and Designed by Hassan Al-khalifah</p>
+      <img src={commentIcon} alt="Comment Icon"/>
+      <img src={githubIcon} alt="Github Icon"/>
+    </div>
   );
 }
 
