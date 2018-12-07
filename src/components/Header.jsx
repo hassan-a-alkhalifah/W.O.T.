4f9808a4d656b422329e535d3b559b8659078c34
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-07 10:05:38 -0800

    add Roboto font family to head tag, add style tag to head tag with which includes styles to remove initial margin and padding on all tags and adds Roboto font family to body tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import homeIcon from '../assets/images/home-icon.png';
+import archiveIcon from '../assets/images/archive-icon.png';
+import finishIcon from '../assets/images/finish-icon.png';
+import deleteIcon from '../assets/images/delete-icon.png';
 
 function Header() {
   return(
-    <div>Header</div>
+    <div>
+      <div>
+        <h1>W.O.T.</h1>
+      </div>
+      <div>
+        <img src={homeIcon} alt="Home Icon"/>
+        <img src={archiveIcon} alt="Archive Icon"/>
+        <img src={finishIcon} alt="Finish Icon"/>
+        <img src={deleteIcon} alt=" Delete Icon"/>
+      </div>
+    </div>
   );
 }
 
