77848615f261fa678987c24bf3434c596273d891
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-07 17:18:56 -0800

    add masterWorkoutList, masterExerciseList and selectedWorkoutId slices of state and pass as props to Workout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,83 @@ import Workout from './Workout';
 import ExerciseArchives from './ExerciseArchives';
 import Footer from './Footer';
 
-function App() {
-  return(
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/' render={()=><Workout />} />
-        <Route exact path='/exerciseArchives' render={()=><ExerciseArchives />} />
-        <Route component={Error404}/>
-      </Switch>
-      <Footer></Footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      masterWorkoutList: {
+        1: {
+          id: null,
+          workoutTitle: '',
+          date: '',
+          workoutNote: '',
+          exerciseList: [
+            1
+          ]
+        }
+      },
+      masterExerciseList: {
+        1: {
+          id: null,
+          exerciseName: '',
+          setList: {
+            1: {
+              id: null,
+              setNumber: 1,
+              weight: '',
+              reps: ''
+            },
+            2: {
+              id: null,
+              setNumber: 2,
+              weight: '',
+              reps: ''
+            }
+          }
+        },
+        2: {
+          id: null,
+          exerciseName: '',
+          setList: {
+            1: {
+              id: null,
+              setNumber: 1,
+              weight: '',
+              reps: ''
+            }
+          }
+        }
+      },
+      selectedWorkoutId: 1
+    };
+  }
+  render() {
+    return(
+      <div>
+        <Header></Header>
+        <Switch>
+          <Route
+            exact path='/'
+            render={()=>
+              <Workout
+                masterWorkoutList={this.state.masterWorkoutList}
+                masterExerciseList={this.state.masterExerciseList}
+                selectedWorkoutId={this.state.selectedWorkoutId}
+              />
+            }
+          />
+          <Route
+            exact path='/exerciseArchives'
+            render={()=>
+              <ExerciseArchives />
+            }
+          />
+          <Route component={Error404}/>
+        </Switch>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
 export default App;
