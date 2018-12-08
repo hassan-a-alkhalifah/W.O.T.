50a7f21f0c5917ba217013f9156f5f88c05992c7
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-07 17:28:30 -0800

    import PropTypes, define setList PropType, map against setList, provide key, setId, setNumber, weight and reps props to Set component

--- src/components/SetList.jsx
+++ src/components/SetList.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Set from './Set';
+import PropTypes from 'prop-types';
 
-function SetList() {
+function SetList(props) {
   return(
     <div>
-      <Set></Set>
+      {Object.keys(props.setList).map((setId) => {
+        let set = props.setList[setId];
+        return(
+          <Set
+            key={setId}
+            setId={setId}
+            setNumber={set.setNumber}
+            weight={set.weight}
+            reps={set.reps}
+          />
+        );
+      })}
     </div>
   );
 }
+
+SetList.propTypes = {
+  setList: PropTypes.object
+};
 
 export default SetList;
