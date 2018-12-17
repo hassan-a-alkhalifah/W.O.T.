e78181ec2d8756927923571f1e00cb7af8f26fa9
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-17 15:51:25 -0800

    add editWorkout and watchFirebaseEditWorkoutRef exportable functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,18 @@ export function addWorkout(workoutTitle, date, workoutNotes, masterExerciseList)
     workoutNotes: workoutNotes,
     masterExerciseList: masterExerciseList
   });
-}
+};
+
+export function editWorkout(selectedWorkoutToBeEditedId, workoutTitle, date, workoutNotes, masterExerciseList) {
+  return function(dispatch) {
+    return workouts.child(selectedWorkoutToBeEditedId).update({
+      workoutTitle: workoutTitle,
+      date: date,
+      workoutNotes: workoutNotes,
+      masterExerciseList: masterExerciseList
+    });
+  };
+};
 
 export function watchFirebaseWorkoutRef() {
   return function(dispatch) {
@@ -23,11 +34,22 @@ export function watchFirebaseWorkoutRef() {
       dispatch(receiveWorkout(newWorkout));
     });
   }
-}
+};
+
+export function watchFirebaseEditWorkoutRef() {
+  return function(dispatch) {
+    workouts.on('child_changed', data => {
+      const newWorkout = Object.assign({}, data.val(), {
+        id: data.getKey()
+      });
+      dispatch(receiveWorkout(newWorkout));
+    });
+  }
+};
 
 function receiveWorkout(workoutFromFirebase) {
   return {
     type: c.RECEIVE_WORKOUT,
     workout: workoutFromFirebase
   };
-}
+};
