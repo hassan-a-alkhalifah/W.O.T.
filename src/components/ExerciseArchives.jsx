436de92aec271ccbe28e1acd3b326c6f23db2717
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-17 12:43:11 -0800

    import PropTypes, define masterWorkoutList propType and pass as prop to PreviousExerciseList component

--- src/components/ExerciseArchives.jsx
+++ src/components/ExerciseArchives.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PreviousExerciseList from './PreviousExerciseList';
+import PropTypes from 'prop-types';
 
-function ExerciseArchives() {
+function ExerciseArchives(props) {
   return(
     <div>
       <div>ExerciseArchives Component</div>
-      <PreviousExerciseList></PreviousExerciseList>
+      <PreviousExerciseList
+        masterWorkoutList={props.masterWorkoutList}
+      />
     </div>
   );
 }
+
+ExerciseArchives.propTypes = {
+  masterWorkoutList: PropTypes.object
+}
+
 
 export default ExerciseArchives;
