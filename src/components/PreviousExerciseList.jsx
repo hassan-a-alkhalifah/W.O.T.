5536f464682598d894a0f987a51b5d06841a141f
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-17 12:45:06 -0800

    import PropTypes, define masterWorkoutList PropType, target object keys from masterWorkoutList and map against it, using the map method, pass workoutTitle, date and id to PreviousExercise componet

--- src/components/PreviousExerciseList.jsx
+++ src/components/PreviousExerciseList.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import PreviousExercise from './PreviousExercise';
+import PropTypes from 'prop-types';
 
-function PreviousExerciseList() {
+function PreviousExerciseList(props) {
   return(
     <div>
-      <div>PreviousExerciseList</div>
-      <PreviousExercise></PreviousExercise>
+      {Object.keys(props.masterWorkoutList).map((workoutId) => {
+        let workout = props.masterWorkoutList[workoutId];
+        return(
+          <PreviousExercise
+            key={workoutId}
+            workoutTitle={workout.workoutTitle}
+            date={workout.date}
+            id={workout.id}
+          />
+        );
+      })}
     </div>
   );
 }
+
+PreviousExerciseList.propTypes = {
+  masterWorkoutList: PropTypes.object
+}
+
 
 export default PreviousExerciseList;
