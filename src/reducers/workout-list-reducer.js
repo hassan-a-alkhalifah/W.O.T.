7ddae1f523584d4d7dff33429fe2a7760e5bc0ce
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-17 11:47:36 -0800

    add RECEIVE_WORKOUT switch case which takes workout from firebase and adds to newState

--- src/reducers/workout-list-reducer.js
+++ src/reducers/workout-list-reducer.js
@@ -2,5 +2,14 @@ import constants from './../constants';
 const { c } = constants;
 
 export default ( state = {}, action ) => {
-  return state;
+
+  switch(action.type) {
+    case c.RECEIVE_WORKOUT:
+      const newState = Object.assign({}, state);
+      newState[action.workout.id] = action.workout;
+      return newState;
+
+    default:
+      return state;
+  }
 };
