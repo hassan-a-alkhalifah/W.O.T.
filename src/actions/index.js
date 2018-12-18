c5a1097f7ea942befa3d458fffd6923bbc5ecc7e
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-17 23:53:23 -0800

    wrap Firebase import and firebase initialization with eslint ignore comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import constants from './../constants';
 const { firebaseConfig, c } = constants;
+/*eslint-disable */
 import Firebase from 'firebase';
 
 firebase.initializeApp(firebaseConfig);
 const workouts = firebase.database().ref('workouts');
+/*eslint-enable */
 
 export function addWorkout(workoutTitle, date, workoutNotes, masterExerciseList) {
   return () => workouts.push({
