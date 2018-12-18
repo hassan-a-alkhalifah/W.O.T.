9351d5d3e2aa6806a26855c43ea293731e457d49
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-18 00:05:03 -0800

    wrap RECEIVE_WORKOUT and default switch cases content in curly braces

--- src/reducers/workout-list-reducer.js
+++ src/reducers/workout-list-reducer.js
@@ -4,12 +4,13 @@ const { c } = constants;
 export default ( state = {}, action ) => {
 
   switch(action.type) {
-  case c.RECEIVE_WORKOUT:
+  case c.RECEIVE_WORKOUT: {
     const newState = Object.assign({}, state);
     newState[action.workout.id] = action.workout;
     return newState;
-
-  default:
+  }
+  default: {
     return state;
+  }
   }
 };
