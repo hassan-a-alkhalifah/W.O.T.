54269803ef488da5095527537da9bf473327ea48
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-12-19 10:14:21 -0800

    add deleteSelectedWorkouts to delete selected workouts from Firebase, add watchFirebaseDeleteWorkoutRef function to render a dispatch method to run deleteWorkout function when any child node is remove from Firebase, add deleteWorkout function to return DELETE_WORKOUT actionType and workoutDeleteId

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,6 +27,15 @@ export function editWorkout(selectedWorkoutToBeEditedId, workoutTitle, date, wor
   };
 }
 
+export function deleteSelectedWorkouts(listOfSelectedWorkoutsToBeDeleted) {
+  return function() {
+    listOfSelectedWorkoutsToBeDeleted.map((workoutToBeDeletedId)=> {
+      workouts.child(workoutToBeDeletedId).remove();
+    });
+    return workouts;
+  }
+}
+
 export function watchFirebaseWorkoutRef() {
   return function(dispatch) {
     workouts.on('child_added', data => {
@@ -49,9 +58,25 @@ export function watchFirebaseEditWorkoutRef() {
   };
 }
 
+export function watchFirebaseDeleteWorkoutRef() {
+  return function(dispatch) {
+    workouts.on('child_removed', data => {
+      dispatch(deleteWorkout(data.getKey()));
+    });
+  };
+}
+
 function receiveWorkout(workoutFromFirebase) {
   return {
     type: c.RECEIVE_WORKOUT,
     workout: workoutFromFirebase
   };
+}
+
+function deleteWorkout(workoutDeletedId) {
+  console.log(workoutDeletedId);
+  return {
+    type: c.DELETE_WORKOUT,
+    workoutDeletedId: workoutDeletedId
+  }
 }
